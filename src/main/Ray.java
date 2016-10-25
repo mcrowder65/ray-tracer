25e6a21678d0dc4f888d0a11be5ff4829f646491
Matt Crowder <mcrowder65@gmail.com>
2016-10-24 20:47:05 -0600

    added ray object

---
+++ src/main/Ray.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Ray {
+	Tuple origin;
+	Tuple direction;
+
+	public Ray(Tuple origin, Tuple direction) {
+		this.origin = origin;
+		this.direction = direction;
+	}
+}
