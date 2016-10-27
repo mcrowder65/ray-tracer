df2def8d9fbdd6f04e1f0181a147c49851a3d6b0
Matt <mcrowder65@gmail.com>
2016-10-27 09:40:01 -0600

    added color and ray. done with last utility video

---
+++ src/utility/Color.java
@@ -0,0 +1,40 @@
+package utility;
+
+public class Color {
+	public float r, g, b;
+
+	public Color() {
+		// default color is black
+		r = 0.0F;
+		g = 0.0F;
+		b = 0.0F;
+	}
+
+	public Color(float r, float g, float b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
+	public Color(Color color) {
+		r = color.r;
+		g = color.g;
+		b = color.b;
+	}
+
+	public void add(Color color) {
+		r += color.r;
+		g += color.g;
+		b += color.b;
+	}
+
+	public void divide(int scalar) {
+		r /= scalar;
+		g /= scalar;
+		b /= scalar;
+	}
+
+	public int toInteger() {
+		return (int) (r * 255) << 16 | (int) (g * 255) << 8 | (int) (b * 255);
+	}
+}
