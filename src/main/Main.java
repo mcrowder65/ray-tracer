8eda942cc73c843f51986e7fad9ba3d3cee8a92c
Matt <mcrowder65@gmail.com>
2016-10-27 10:29:57 -0600

    got first circle drawing!

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,12 @@ import java.io.File;
 
 import javax.imageio.ImageIO;
 
+import geometry.Sphere;
+import utility.Color;
+import utility.Point3D;
+import utility.Ray;
+import utility.Vector3D;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -13,19 +19,28 @@ public class Main {
 		int height = 900;
 		File image = new File("Image.png");
 		BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+		Sphere sphere = new Sphere(new Point3D(0.0, 0.0, 0.0), 60.0, new Color(1.0F, 0.0F, 0.0F));
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
+				Ray ray = new Ray(new Point3D(x - width / 2 + .5, y - height / 2 + .5, 100),
+						new Vector3D(0.0, 0.0, -1.0));
+				if (sphere.hit(ray) != 0.0) {
+					buffer.setRGB(x, y, sphere.color.toInteger());
+				} else {
+					buffer.setRGB(x, y, 0);
+				}
 
 			}
 		}
 		try {
 			ImageIO.write(buffer, "PNG", image);
+			long end = System.nanoTime();
+			System.out.println("Loop Time: " + (end - start) / 1000000000.0F);
 		} catch (Exception e) {
 			System.out.println("could not write image.");
 		}
 
-		long end = System.nanoTime();
-		System.out.println("Loop Time: " + (end - start) / 1000000000.0F);
 	}
 
 }
